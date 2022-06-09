34114ace2e5b578f5f9f4cf267aa395abdadeed6
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-09 17:08:16 +0300

    Update Laravel: Add vue, js file for module Gaz

--- app/app/Modules/GReqSys/Views/index.blade.php
+++ app/app/Modules/GReqSys/Views/index.blade.php
@@ -1,15 +1,13 @@
 @extends('GReqSys::tpl.main')
 
 @section('body')
-
-<section class="site-section">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>GazReqSys</h1>
+    <section class="site-section">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>GazReqSys</h1>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-
+    </section>
 @endsection
