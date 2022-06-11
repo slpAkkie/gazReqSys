bf2633ce4b48dd27deaec342431c84d5376de072
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-12 00:55:51 +0300

    Update GReqSys: Add req creating

--- app/Modules/GReqSys/Controllers/ReqController.php
+++ app/Modules/GReqSys/Controllers/ReqController.php
@@ -4,7 +4,10 @@ namespace Modules\GReqSys\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Modules\Gaz\Models\City;
+use Modules\Gaz\Models\Stuff;
+use Modules\GReqSys\Models\InvolvedStuff;
 use Modules\GReqSys\Models\Req;
 use Modules\GReqSys\Models\ReqType;
 use Modules\GReqSys\Requests\StoreReqRequest;
@@ -42,6 +45,31 @@ class ReqController extends Controller
      * @return void
      */
     public function store(StoreReqRequest $request) {
+        ($req = new Req($request->only([
+            'type_id',
+            'department_id',
+        ])))->save();
 
+        $emp_numbers = Collection::make($request->get('stuff'))->reduce(function ($r, $v) {
+            $r[] = $v['emp_number'];
+
+            return $r;
+        } ,[]);
+
+        $req->involved_stuff_records()->createMany(
+            Stuff::select('stuff.id')->whereIn('emp_number', $emp_numbers)->whereHas('departments', function ($q) use ($request) {
+                $q->where('departments.id', $request->get('department_id'));
+            })->get()->reduce(function ($r, $v) {
+                $r[] = [
+                    'gaz_stuff_id' => $v['id'],
+                ];
+
+                return $r;
+            }, [])
+        )->each(function ($model) {
+            $model->save();
+        });
+
+        return response()->json();
     }
 }
--- app/Modules/GReqSys/Models/Req.php
+++ app/Modules/GReqSys/Models/Req.php
@@ -2,8 +2,11 @@
 
 namespace Modules\GReqSys\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Auth;
 use Modules\Gaz\Models\Department;
 use Modules\Gaz\Models\Stuff;
 use Modules\GReqSys\Models\Model;
@@ -18,6 +21,7 @@ use Modules\GReqSys\Models\Model;
  * @property ReqType $type
  * @property User $author
  * @property Stuff $stuff
+ * @property Collection<Stuff> $involved_stuff
  * @property Department $department
  *
  * @mixin Builder
@@ -34,6 +38,18 @@ class Req extends Model
         'user_id',
         'gaz_department_id',
     ];
+
+    public function __construct(array $attributes = [])
+    {
+        if (key_exists('department_id', $attributes)) {
+            $attributes['gaz_department_id'] = $attributes['department_id'];
+            unset($attributes['department_id']);
+        }
+
+        $attributes['user_id'] = Auth::id();
+
+        parent::__construct($attributes);
+    }
 
     /**
      * Получить типа заявки
@@ -63,6 +79,26 @@ class Req extends Model
     public function stuff()
     {
         return $this->author->stuff();
+    }
+
+    /**
+     * Сотрудники, вовлеченные в эту заявку
+     *
+     * @return HasMany
+     */
+    public function involved_stuff()
+    {
+        return $this->setConnection('gaz')->hasMany(Stuff::class, 'req_id', 'id');
+    }
+
+    /**
+     * Записи о вовлеченных сотрудниках
+     *
+     * @return HasMany
+     */
+    public function involved_stuff_records()
+    {
+        return $this->hasMany(InvolvedStuff::class, 'req_id', 'id');
     }
 
     /**
