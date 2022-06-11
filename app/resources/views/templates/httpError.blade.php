bfb8c911aef8bbdaaf07850f3445acbe70f84cca
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-11 13:10:07 +0300

    Update httpError blade template

--- app/resources/views/templates/httpError.blade.php
+++ app/resources/views/templates/httpError.blade.php
@@ -4,7 +4,12 @@
     <div class="http-error p-2">
         <h1 class="http-error__code text-danger mb-0 me-4">@yield('code')</h1>
         <div class="http-error__body">
-            <p class="http-error__title text-danger mb-1">@yield('title')</p>
+            <p class="http-error__title text-danger mb-1">
+                @if(!!$exception->getMessage())
+                    {{ $exception->getMessage() }}
+                @else
+                    @yield('title')
+                @endif</p>
             <p class="http-error__message text-muted">@yield('message')</p>
         </div>
     </div>
