9308715806d6516b8228633818935cff9a8df1d6
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-18 02:19:40 +0300

    Update: add toggler to show the password

--- app/Modules/ReqSys/Views/login.blade.php
+++ app/Modules/ReqSys/Views/login.blade.php
@@ -10,15 +10,16 @@
             @csrf
             <div class="mb-2">
                 <label class="form-label form-label_prefix-icon d-flex align-items-center [@error('login') is-invalid @enderror]" for="login">
-                    <i class="bi bi-person-fill me-2 form-control-icon"></i>
+                    <i class="bi bi-person-fill me-2 form-control-icon form-control-icon_prefix"></i>
                     <input type="text" id="login" name="login" class="form-control form-control_prefix-icon w-100 border-c-light prefix-icon" placeholder="Логин" v-model="formData.login">
                 </label>
                 <p class="invalid-feedback">@error('login') {{ $message }} @enderror</p>
             </div>
             <div class="mb-2">
                 <label class="form-label form-label_prefix-icon d-flex align-items-center [@error('password') is-invalid @enderror]" for="password">
-                    <i class="bi bi-key-fill me-2 form-control-icon"></i>
-                    <input type="password" id="password" name="password" class="form-control form-control_prefix-icon w-100 border-c-light" placeholder="Пароль" v-model="formData.password">
+                    <i class="bi bi-key-fill form-control-icon form-control-icon_prefix"></i>
+                    <input :type="passwordInputType" id="password" name="password" class="form-control form-control_prefix-icon form-control_suffix-icon w-100 border-c-light" placeholder="Пароль" v-model="formData.password">
+                    <i class="bi form-control-icon form-control-icon_suffix cursor-pointer" :class="passwordShowIcon" @click="showPassword = !showPassword"></i>
                 </label>
                 <p class="invalid-feedback">@error('password') {{ $message }} @enderror</p>
             </div>
@@ -46,7 +47,17 @@
                     password: 16,
                 },
                 formBlocked: false,
+
+                showPassword: false,
             }),
+            computed: {
+                passwordInputType() {
+                    return this.showPassword ? 'text' : 'password'
+                },
+                passwordShowIcon() {
+                    return this.showPassword ? 'bi-eye-slash' : 'bi-eye'
+                },
+            },
             watch: {
                 'formData.login': function (newVal, oldVal) {
                     (newVal.length > this.maxLength.login) && (this.formData.login = oldVal)
