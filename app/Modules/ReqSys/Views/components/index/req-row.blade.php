5512465a6795164b1be48ee9f2f2808f35e2e137
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-19 00:59:55 +0300

    Update ReqSys: req-row status hightlighting

--- app/Modules/ReqSys/Views/components/index/req-row.blade.php
+++ app/Modules/ReqSys/Views/components/index/req-row.blade.php
@@ -1,13 +1,21 @@
 <a href="{{ route('req.show', $req->id) }}" class="table__row">
     <div class="col-2 table__cell req-table__cell_number-and-date">
-        @if($req->status->slug === 'confirmed')
-            <span class="fw-bold border-success border text-success rounded p-1">#{{ $req->id }} </span>
-        @elseif($req->status->slug === 'denied')
-            <span class="fw-bold border-danger border text-danger rounded p-1">#{{ $req->id }} </span>
-        @else
-            <span class="fw-bold border-black border rounded p-1">#{{ $req->id }} </span>
-        @endif
-         от <span>{{ $req->created_at }}</span>
+        <span class="fw-bold rounded p-1
+            @switch($req->status->slug)
+                @case('waiting')
+                    border border-black
+                    @break
+                @case('confirmed')
+                    bg-success border border-success text-light
+                    @break
+                @case('resolved')
+                    bg-primary border border-primary
+                    @break
+                @case('denied')
+                    bg-danger border border-danger text-light
+                    @break
+            @endswitch
+        ">#{{ $req->id }}</span> от <span>{{ $req->created_at }}</span>
     </div>
     <div class="col-3 table__cell">{{ $req->organization->title }}</div>
     <div class="col-2 table__cell">{{ $req->organization->city->title }}</div>
