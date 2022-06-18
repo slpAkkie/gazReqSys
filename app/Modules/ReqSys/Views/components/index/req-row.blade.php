056b5ccd7daae395611d94adfbd3ffb564d5e609
R4GUDA <66072295+R4GUDA@users.noreply.github.com>
2022-06-18 23:58:29 +0300

    Paint req id

--- app/Modules/ReqSys/Views/components/index/req-row.blade.php
+++ app/Modules/ReqSys/Views/components/index/req-row.blade.php
@@ -1,6 +1,13 @@
 <a href="{{ route('req.show', $req->id) }}" class="table__row">
     <div class="col-2 table__cell req-table__cell_number-and-date">
-        <span class="fw-bold">#{{ $req->id }} </span> от <span>{{ $req->created_at }}</span>
+        @if($req->status->slug === 'confirmed')
+            <span class="fw-bold border-success border text-success rounded p-1">#{{ $req->id }} </span>
+        @elseif($req->status->slug === 'denied')
+            <span class="fw-bold border-danger border text-danger rounded p-1">#{{ $req->id }} </span>
+        @else
+            <span class="fw-bold border-black border rounded p-1">#{{ $req->id }} </span>
+        @endif
+         от <span>{{ $req->created_at }}</span>
     </div>
     <div class="col-3 table__cell">{{ $req->organization->title }}</div>
     <div class="col-2 table__cell">{{ $req->organization->city->title }}</div>
